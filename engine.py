7a950f4b7246306d1a53a133e56fa7ffef134f49
Barry Warsaw <barry@python.org>
2003-04-15 21:14:39 +0000

    ZopeContext.translate(): Set mapping default value to None.  When msgid is a MessageID, the mapping tags along with the object so it needn't be specified.
    Ideally, I'd like to change the signature of this message so that
    msgid comes first and domain defaults to None.  The domain tags along
    with MessageIDs too, so they needn't be always specified.

--- engine.py
+++ engine.py
@@ -15,7 +15,7 @@
 
 Each expression engine can have its own expression types and base names.
 
-$Id: engine.py,v 1.10 2003/04/14 12:15:50 matth Exp $
+$Id: engine.py,v 1.11 2003/04/15 21:14:39 bwarsaw Exp $
 """
 __metaclass__ = type # All classes are new style when run with Python 2.2+
 
@@ -64,7 +64,9 @@ class ZopeContext(Context):
         macro = removeAllProxies(macro)
         return macro
 
-    def translate(self, domain, msgid, mapping, default=None):
+    # XXX msgid should come first, with domain defaulting to None.  When msgid
+    # is a MessageID, domain tags along and needn't be specified.
+    def translate(self, domain, msgid, mapping=None, default=None):
         # When running Zope, request is a Proxy, but no mutation is done here,
         # so it is safe to remove all proxies
         request = removeAllProxies(self.request)
