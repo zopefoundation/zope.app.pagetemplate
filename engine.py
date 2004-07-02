4d703e09e3a866feeccb3518a6ec95155a012566
Jim Fulton <jim@zope.com>
2004-07-02 21:00:18 +0000

    Merged from trunk 25974 and 25984:
    Moved _error_start to module rather than class scope, so as not to
    mistakingly thing that it's part of a public or subclassing interface.
    
    Removed the unused _engine_name.
    
    Redefined pt_getEngineContext as a regular method.
    
    Resolved an XXX
    
    Inlined the barely use html method.
    
    
    Fixed bug publishing bound page templates
    
    When Fred and I added an interface declaration to
    zope.pagetemplate.pagetemplate.PageTemplate, we unwittingly broke
    BoundPageTemplates. This was due to the fact that BoundPageTemplates
    were overagressive about exposing underlying template attributes, 
    including interface declarations.  
    
    Changed BoundPageTemplates to only expose needed attributes, macros
    amd filename.
    
    Added a test for the view that exposed the error.

---
+++ engine.py
@@ -0,0 +1,208 @@
+##############################################################################
+#
+# Copyright (c) 2002 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+"""Expression engine configuration and registration.
+
+Each expression engine can have its own expression types and base names.
+
+$Id$
+"""
+import sys
+from types import StringTypes
+
+from zope.tales.expressions import PathExpr, StringExpr, NotExpr, DeferExpr
+from zope.tales.pythonexpr import PythonExpr
+from zope.tales.tales import ExpressionEngine, Context
+
+from zope.component.exceptions import ComponentLookupError
+from zope.proxy import removeAllProxies
+from zope.security.proxy import ProxyFactory
+from zope.security.builtins import RestrictedBuiltins
+from zope.i18n import translate
+
+from zope.app import zapi
+from zope.app.i18n import ZopeMessageIDFactory as _
+from zope.app.traversing.adapters import Traverser
+from zope.app.traversing.interfaces import IPathAdapter
+
+class InlineCodeError(Exception):
+    pass
+
+def zopeTraverser(object, path_items, econtext):
+    """Traverses a sequence of names, first trying attributes then items.
+    """
+    traverser = Traverser(object)
+    return traverser.traverse(path_items,
+                              request=getattr(econtext, 'request', None))
+
+
+class ZopePathExpr(PathExpr):
+
+    def __init__(self, name, expr, engine):
+        super(ZopePathExpr, self).__init__(name, expr, engine, zopeTraverser)
+
+class ZopePythonExpr(PythonExpr):
+
+    def __call__(self, econtext):
+        __traceback_info__ = self.text
+        vars = self._bind_used_names(econtext, RestrictedBuiltins)
+        return eval(self._code, vars)
+
+class ZopeContext(Context):
+
+    def setContext(self, name, value):
+        # Hook to allow subclasses to do things like adding security proxies
+        Context.setContext(self, name, ProxyFactory(value))
+
+    def evaluateText(self, expr):
+        text = self.evaluate(expr)
+        if text is self.getDefault() or text is None:
+            return text
+        if isinstance(text, StringTypes):
+            # text could be a proxied/wrapped object
+            return text
+        return unicode(text)
+
+    def evaluateMacro(self, expr):
+        macro = Context.evaluateMacro(self, expr)
+        macro = removeAllProxies(macro)
+        return macro
+
+    def translate(self, msgid, domain=None, mapping=None, default=None):
+        # When running Zope, request is a Proxy, but no mutation is done here,
+        # so it is safe to remove all proxies
+        request = removeAllProxies(self.request)
+        return translate(self.context, msgid, domain, mapping,
+                         context=request, default=default)
+
+    evaluateInlineCode = False
+
+    def evaluateCode(self, lang, code):
+        if not self.evaluateInlineCode:
+            raise InlineCodeError, \
+                  _('Inline Code Evaluation is deactivated, which means that '
+                    'you cannot have inline code snippets in your Page '
+                    'Template. Activate Inline Code Evaluation and try again.')
+
+        # XXX This is only needed when self.evaluateInlineCode is true,
+        # so should only be needed for zope.app.pythonpage.
+        from zope.app.interpreter.interfaces import IInterpreter
+        interpreter = zapi.queryUtility(IInterpreter, lang)
+        if interpreter is None:
+            error = _('No interpreter named "${lang_name}" was found.')
+            error.mapping = {'lang_name': lang}
+            raise InlineCodeError, error
+                  
+        globals = self.vars.copy()
+        result = interpreter.evaluateRawCode(code, globals)
+        # Add possibly new global variables.
+        old_names = self.vars.keys()
+        for name, value in globals.items():
+            if name not in old_names:
+                self.setGlobal(name, value)
+        return result
+
+
+class AdapterNamespaces(object):
+    """Simulate tales function namespaces with adapter lookup.
+
+    When we are asked for a namespace, we return an object that
+    actually computes an adapter when called:
+
+    To demonstrate this, we need to register an adapter:
+
+      >>> from zope.app.tests.placelesssetup import setUp, tearDown
+      >>> setUp()
+      >>> from zope.app.tests import ztapi
+      >>> def adapter1(ob):
+      ...     return 1
+      >>> ztapi.provideAdapter(None, IPathAdapter, adapter1, 'a1')
+
+    Now, with this adapter in place, we can try out the namespaces:
+
+      >>> ob = object()
+      >>> namespaces = AdapterNamespaces()
+      >>> namespace = namespaces['a1']
+      >>> namespace(ob)
+      1
+      >>> namespace = namespaces['a2']
+      >>> namespace(ob)
+      Traceback (most recent call last):
+      ...
+      KeyError: 'a2'
+
+
+    Cleanup:
+    
+      >>> tearDown()
+    """
+
+    def __init__(self):
+        self.namespaces = {}
+
+    def __getitem__(self, name):
+        namespace = self.namespaces.get(name)
+        if namespace is None:
+            def namespace(object):
+                try:
+                    return zapi.getAdapter(object, IPathAdapter, name=name)
+                except ComponentLookupError:
+                    raise KeyError, name
+                
+            self.namespaces[name] = namespace
+        return namespace
+
+class ZopeEngine(ExpressionEngine):
+
+    def __init__(self):
+        ExpressionEngine.__init__(self)
+        self.namespaces = AdapterNamespaces()
+
+    def getContext(self, __namespace=None, **namespace):
+        if __namespace:
+            if namespace:
+                namespace.update(__namespace)
+            else:
+                namespace = __namespace
+
+        context = ZopeContext(self, namespace)
+
+        # Put request into context so path traversal can find it
+        if 'request' in namespace:
+            context.request = namespace['request']
+
+        # Put context into context so path traversal can find it
+        if 'context' in namespace:
+            context.context = namespace['context']
+
+        return context
+
+def _Engine(engine=None):
+    if engine is None:
+        engine = ZopeEngine()
+        
+    for pt in ZopePathExpr._default_type_names:
+        engine.registerType(pt, ZopePathExpr)
+    engine.registerType('string', StringExpr)
+    engine.registerType('python', ZopePythonExpr)
+    engine.registerType('not', NotExpr)
+    engine.registerType('defer', DeferExpr)
+    engine.registerBaseName('modules', ProxyFactory(sys.modules))
+    return engine
+
+Engine = _Engine()
+
+class AppPT(object):
+
+    def pt_getEngine(self):
+        return Engine
