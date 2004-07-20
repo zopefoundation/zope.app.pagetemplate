ba9f1f5d9411bf0f758dee94d6b3ac0236d8b7fc
Fred Drake <fdrake@acm.org>
2004-07-20 20:01:05 +0000

    clean up some boilerplate

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -19,9 +19,7 @@ import unittest
 from zope.testing.doctestunit import DocTestSuite
 
 def test_suite():
-    return unittest.TestSuite((
-        DocTestSuite('zope.app.pagetemplate.engine'),
-        ))
+    return DocTestSuite('zope.app.pagetemplate.engine')
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
