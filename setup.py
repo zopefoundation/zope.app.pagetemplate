ce1ce3a3dddcb5034a59fe4dd607a64e972b0e68
Fred Drake <fdrake@acm.org>
2007-03-25 10:34:23 +0000

    - start buildoutification - make some progress on dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
-import os
-
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.app.pagetemplate',
-      version='0.1dev',
+      version='0.1',
       url='http://svn.zope.org/zope.app.pagetemplate',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
 
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
 
       namespace_packages=['zope', 'zope.app'],
-      include_package_data = True,
-
-      zip_safe = False,
+      include_package_data=True,
+      install_requires=[
+          'setuptools',
+          'zope.pagetemplate',
+          ],
+      extras_require={
+          "test": [
+              "zope.app.authentication",
+              "zope.app.container",
+              "zope.app.form",
+              "zope.app.securitypolicy",
+              "zope.app.zcmlfiles",
+              ],
+          },
+      zip_safe=False,
       )
