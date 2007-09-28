57701fec28393eec306ee41adde9afc17a7fe1c0
Philipp von Weitershausen <philipp@weitershausen.de>
2007-09-28 11:22:21 +0000

    prepare release

---
+++ README.txt
@@ -0,0 +1,21 @@
+The ``zope.app.pagetemplate`` package integrates the Page Template
+templating system (``zope.pagetemplate``) into the Zope 3 application
+server.  In particular, it provides:
+
+* a TALES engine implementation that uses Zope's security system for
+  checking access,
+
+* a ``ViewPageTemplateFile`` class that can be put on a browser page
+  class as an attribute and will function as a callable method whose
+  result is the rendered template, e.g.::
+
+    from zope.publisher import BrowserPage
+    from zope.app.pagetemplate import ViewPageTemplateFile
+
+    class HelloWorldPage(BrowserPage):
+        __call__ = ViewPageTemplateFile('helloworld.pt')
+
+* TALES namespace adapters for easy access to DublinCore metadata
+  (e.g. ``obj/zope:title``) and URL quoting
+  (e.g. ``obj/@@absolute_url/url:quote``).
+
--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup, find_packages
 
+long_description = (open('README.txt').read() +
+                    '\n\n' +
+                    open('CHANGES.txt').read())
+
 setup(name='zope.app.pagetemplate',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.app.pagetemplate',
+      version = '3.4.0',
+      url='http://pypi.python.org/pypi/zope.app.pagetemplate',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
+      description='PageTemplate integration for Zope 3',
+      long_description=long_description,
+      classifiers=['Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Zope Public License',
+                   'Programming Language :: Python',
+                   'Framework :: Zope3',
+                   ],
 
       packages=find_packages('src'),
       package_dir={'': 'src'},
-
       namespace_packages=['zope', 'zope.app'],
       include_package_data=True,
       install_requires=[
