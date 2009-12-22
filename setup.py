7fb0609a06ae35b2ac08fa8fc9043dd9d327be47
Hanno Schlichting <hanno@hannosch.eu>
2009-12-22 21:43:43 +0000

    Prepare zope.app.pagetemplate 3.10.0.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import os.path
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '3.10.0dev'
+version = '3.10.0'
 
 
 setup(name='zope.app.pagetemplate',
