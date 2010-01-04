6ebab91e9a3734293f2ed28add4f8316201c9d96
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2010-01-04 14:14:45 +0000

    Back to development: 3.10.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import os.path
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '3.10.1'
+version = '3.10.2dev'
 
 
 setup(name='zope.app.pagetemplate',
